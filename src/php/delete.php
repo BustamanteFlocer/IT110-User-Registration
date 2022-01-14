0e3b88707e01ce0a07a8f4bae6d1275b2d2370b5
BustamanteFlocer <77340285+BustamanteFlocer@users.noreply.github.com>
2022-01-15 02:28:06 +0800

    Update delete.php

--- src/php/delete.php
+++ src/php/delete.php
@@ -6,8 +6,7 @@ $id  = $_POST['userID'];
 // Delete selected user from the database
 $sql = "DELETE FROM user WHERE id = '".$id."'";
 $statement = $pdo->query($sql);
-$users = $statement->fetchAll(PDO::FETCH_ASSOC);
-		echo json_encode($users);
+
 	
 ?>
 
